5318cbc298f6c254dd77cdc41db53b93242e6827
brano12 <112586043+brano12@users.noreply.github.com>
2026-05-19 17:41:10 +0200

    Update index.js
    Pages functionality and simplifying some functions

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const OpenedDoc = window.location.href.split("/").pop().slice(0, -5)
+const openedDoc = window.location.href.split("/").pop().slice(0, -5);
+const pageNumber = document.querySelector('.page-number');
+const leftPanelBttns = Array.from(document.getElementsByClassName('left-panel')[0].children);
+const openedIMG = document.querySelector('.main-img');
+const prevBtn = document.getElementsByClassName('main-panel-buttons')[0];
+const nextBtn = document.getElementsByClassName('main-panel-buttons')[1];
 
-const LeftPanelBttns = Array.from(document.getElementsByClassName('left-panel')[0].children)
-const OpenedIMG = document.getElementsByClassName('main-img')[0]
-const PrevBtn = document.getElementsByClassName('main-panel-buttons')[0]
-const NextBtn = document.getElementsByClassName('main-panel-buttons')[1]
+openedIMG.src = `${openedDoc}/${leftPanelBttns[0].innerText.toLowerCase()} 1.png`;
 
-OpenedIMG.src = `${OpenedDoc}/${LeftPanelBttns[0].innerText} 1.png`
+function prepareImageString(filename) {
+
+  let openedIMGstring = filename.split("/").pop().slice(0,-4);
+  let openedIMGArray =  openedIMGstring.split("%20");
+
+  if (openedIMGArray.length == 1) {
+    openedIMGArray =  openedIMGstring.split(" ");
+  }
+
+  let category = "";
+  let index;
+
+  openedIMGArray.forEach(item => {
+    if(!Number(item)){
+      category = category + " " + item;
+    }
+  });
+
+  category = category.trim()
+  index = Number(openedIMGArray[openedIMGArray.length - 1]);
+  return [category, index];
+}
+
+pageNumber.innerText = prepareImageString(openedIMG.src)[0] + " pg." + prepareImageString(openedIMG.src)[1]
+
+
 
 
 function imageExists(url) {
@@ -19,93 +46,95 @@ function imageExists(url) {
   });
 }
 
+
 async function indexSearch(indx,condition, categ) {
   do {
     indx += 1;
-    const exists = await imageExists(`${OpenedDoc}/${categ} ${indx}.png`);
+    const exists = await imageExists(`${openedDoc}/${categ} ${indx}.png`);
 
     if (exists === condition) {
       break;
     }
-  } while (true);
+  } while (indx < 200);
   return indx;
 }
 
 
-LeftPanelBttns.forEach(button => {
+leftPanelBttns.forEach(button => {
 
   button.addEventListener("click", async () => {
 
-    let index = await indexSearch(0, true, button.innerText);
-    OpenedIMG.src = `${OpenedDoc}/${button.innerText} ${index}.png`
-
+    let index = await indexSearch(0, true, button.innerText.toLowerCase());
+    openedIMG.src = `${openedDoc}/${button.innerText.toLowerCase()} ${index}.png`;
+    pageNumber.innerText = prepareImageString(openedIMG.src)[0] + " pg." + prepareImageString(openedIMG.src)[1]
   });
 });
 
 
-async function findPrevIMG(url, index, category) {
-  const exists = await imageExists(url);
+async function categoryCheck(category, index, direction){
 
-  if (exists) {
-    OpenedIMG.src = url;
+    let btnIndx = leftPanelBttns.findIndex(item => item.innerText.toLowerCase() === category);
 
-  } else {
-    let btnIndx = LeftPanelBttns.findIndex(item => item.innerText === category);
+      //checking whether we are not at the beginning of the slide show - first LeftPanelBttns category
+    if (btnIndx == 0 && index == 0 && direction == -1) {
 
-    //checking whether we are not at the beginning of the slide show - first LeftPanelBttns category
-    if (btnIndx == 0) {
-      category = LeftPanelBttns[LeftPanelBttns.length - 1].innerText;
+      category = leftPanelBttns[leftPanelBttns.length - 1].innerText.toLowerCase();
       //we will switch to highest category - last LeftPanelBttn
       //and we need to find lowest and then highest index of highest category
       index = await indexSearch(0, true, category)
       index = await indexSearch(index, false, category)
       index = index-1;
-
-    } else {
-      category = LeftPanelBttns[btnIndx - 1].innerText;
     }
-      OpenedIMG.src = `${OpenedDoc}/${category} ${index}.png`
-  }
+
+  //checking whether we are not at the end of the slide show - last LeftPanelBttns category
+    else if (leftPanelBttns.length == (btnIndx+1) && direction == 1) {
+
+      category = leftPanelBttns[0].innerText.toLowerCase();
+      index = 1;
+    } else {
+      category = leftPanelBttns[btnIndx + direction].innerText.toLowerCase();
+
+      }
+
+    return [category, index];
 }
 
 
-PrevBtn.addEventListener("click", () =>{
-  let openedIMGstring = OpenedIMG.src.split("/").pop().slice(0,-4);
-  let openedIMGArray =  openedIMGstring.split("%20");
-  let category = openedIMGArray[0];
-  let index = openedIMGArray[1];
-  index -= 1;
+async function findIMG(category, index, direction) {
 
-  findPrevIMG(`${OpenedDoc}/${category} ${index}.png`, index, category)
+  const exists = await imageExists(`${openedDoc}/${category} ${index}.png`);
+
+  if (!exists) {
+    let catIndxArray = await categoryCheck(category, index, direction);
+
+    return catIndxArray;
+  }
+
+  return [category, index];
+}
+
+
+
+// direction PREVIOUS is represented with value -1 and NEXT with 1
+prevBtn.addEventListener("click", async () =>{
+
+  category = prepareImageString(openedIMG.src)[0] ;
+  index = prepareImageString(openedIMG.src)[1] - 1;
+
+  let findIMGarray = await findIMG(category, index, -1)
+
+  openedIMG.src = `${openedDoc}/${findIMGarray[0]} ${findIMGarray[1]}.png`;
+  pageNumber.innerText = findIMGarray[0] + " pg." + findIMGarray[1]
 });
 
 
-async function findNextIMG(url, index, category) {
-  const exists = await imageExists(url);
+nextBtn.addEventListener("click", async () =>{
 
-  if(exists){
-      OpenedIMG.src = url;
+  category = prepareImageString(openedIMG.src)[0] ;
+  index = prepareImageString(openedIMG.src)[1] + 1;
 
-  } else {
-    let btnIndx = LeftPanelBttns.findIndex( item =>item.innerText === category);
-    //checking whether we are not at the end of the slide show - last LeftPanelBttns category
-    if(LeftPanelBttns.length == (btnIndx+1)){
-      category = LeftPanelBttns[0].innerText;
-      index = 1;
-    }else{
-      category = LeftPanelBttns[btnIndx+1].innerText;
-    }
+  let findIMGarray = await findIMG(category, index, 1)
 
-    OpenedIMG.src = `${OpenedDoc}/${category} ${index}.png`
-  }
-}
-
-NextBtn.addEventListener("click", () =>{
-  let openedIMGstring = OpenedIMG.src.split("/").pop().slice(0,-4);
-  let openedIMGArray =  openedIMGstring.split("%20");
-  let category = openedIMGArray[0];
-  let index = Number(openedIMGArray[1]);
-  index += 1;
-
-  findNextIMG(`${OpenedDoc}/${category} ${index}.png`, index, category)
+  openedIMG.src = `${openedDoc}/${findIMGarray[0]} ${findIMGarray[1]}.png`;
+  pageNumber.innerText = findIMGarray[0] + " pg." + findIMGarray[1]
 });
